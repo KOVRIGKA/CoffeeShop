c8623dfbddef47cd7bf5a7e050b9af5bb4ca04ca
Анастасия Ковригина <anastasiakovrigina@MacBook-Pro-Anastasia.local>
2019-10-10 16:57:06 +0300

    first

--- js/main.js
+++ js/main.js
@@ -8,24 +8,25 @@ const button = document.querySelectorAll('button[data-type]');
 
 for(let i = 0; i < button.length-1; i++){
     button[i].addEventListener('click', function(){
-        let type = button.dataset.type;
-        return type;
+        console.log(button[i].dataset.type);
+        let type = button[i].dataset.type;
+        Block(type);
+        // return type;
     });
   }
 
-function Block(){
+function Block(type){
     document.querySelector('.pay').disabled = false;
     if (type === 'simple'){
-        document.querySelectorAll('button[data-type='author']').disabled = true;
-        document.querySelector('button[data-type='syrup']').disabled = false;
+        document.querySelectorAll('button[data-type=author]').disabled = true;
+        document.querySelector('button[data-type=syrup]').disabled = false;
     }
     else if (type === 'author'){
         document.querySelectorAll('button[data-type]').disabled = true;
-    }
-    else (type === 'milk'){
-        document.querySelectorAll('button[data-type='simple']').disabled = true;
-        document.querySelectorAll('button[data-type='author']').disabled = true;
-        document.querySelector('button[data-type='syrup']').disabled = false;
+    }else if(type === 'milk'){
+        document.querySelectorAll('button[data-type=simple]').disabled = true;
+        document.querySelectorAll('button[data-type=author]').disabled = true;
+        document.querySelector('button[data-type=syrup]').disabled = false;
     }
 }
 
